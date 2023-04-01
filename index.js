9ad2ab6c93cfbd494356f878eebc6ac3046d6091
Giashuddin786 <giashuddin4740@gmail.com>
2023-04-01 22:31:43 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,12 @@
+
+var navsty= document.getElementById('navStyle');
+			function showMenu() {
+				navsty.style.right = "0";
+				
+			}
+			function hideMenu(){
+				navsty.style.right = "-200px"
+			}
+
+
+		
